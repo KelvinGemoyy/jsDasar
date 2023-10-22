710b61462eaed264d329fc408ee6f852796cec35
kelvin <muhammadfalahalfarisi@gmail.com>
2023-10-23 07:35:58 +0800

    tes tes...

---
+++ All.js
@@ -0,0 +1,12 @@
+//Function Expression
+// const tampilNama = function (nama) {
+//     return `Halo,${nama}`;
+// }
+
+// console.log(tampilNama('kelvin'));
+
+const tampilNama = (nama) => {
+    return `Halo,${nama}`
+};
+
+console.log(tampilNama('Kelvin'));
